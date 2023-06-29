e63cd53fbf42e32adc02642005a6e892760b1457
Isaac-kaberere <isaacbashco@gmail.com>
2023-06-29 12:41:25 +0300

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-void jack_bauer(void) {
+void jack_bauer(void){
     int hour, minute;
 
-    for (hour = 0; hour < 24; hour++) {
-        for (minute = 0; minute < 60; minute++) {
-            int hour_tens = hour / 10;
-            int hour_ones = hour % 10;
-            int minute_tens = minute / 10;
-            int minute_ones = minute % 10;
-
-            printf("%d%d:%d%d\n", hour_tens, hour_ones, minute_tens, minute_ones);
+    for (hour = 0; hour < 24; hour++){
+        for (minute = 0; minute < 60; minute++){
+            printf("%02d:%02d\n", hour, minute);
         }
     }
-}
 
-int main(void) {
+}
+int main(void){
     jack_bauer();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
