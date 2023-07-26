b88c550655aa225a13d35dc299d06a4407e71503
Isaac-kaberere <isaacbashco@gmail.com>
2023-07-26 15:17:36 +0300

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
-int add(int, int);
+int add(int a, int b){
+    int c = a + b;
+    return c;
+}
 
 int main(){
-    int a,b;
-    int result;
+    int a, b;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
 
-    printf("Enter a number: ");
-    scanf("%d%d", &a, &b);
+    int result = add(a, b);
 
-    result = add(a, b);
-    printf("THe sum of %d and %d is: %d\n", a, b, result);
+    printf("The sum of %d and %d is %d\n", a, b, result);
 
     return 0;
-
-
-
 }
-int add(int a, int b){
-    int sum;
 
-    sum = a+b;
-
-    return sum;
-
-}
